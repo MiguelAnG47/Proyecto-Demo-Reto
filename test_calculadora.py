949ff4bc1fdb926a93c8134c828d5adc9d8728b3
MiguelAnG47 <miguelafanquin10@gmail.com>
2025-10-31 15:38:54 -0500

    Agregado flujo CI con pruebas automáticas

---
+++ test_calculadora.py
@@ -0,0 +1,13 @@
+# test_calculadora.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def test_sumar():
+    assert sumar(3, 4) == 7
+
+def test_restar():
+    assert restar(10, 3) == 7
